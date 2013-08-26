b83aa38606e47cbfe10a2de319a0e24a5a0d3df2
dennis <bak723@gmail.com>
2013-08-26 20:21:08 +0900

    모르겠다....

--- src/diamond_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef DIAMOND_UTILS_H
-#define DIAMOND_UTILS_H
-
-namespace DIAMOND_NS {
-std::string toString(legacy_db_type dbType);
-handlerton *getHandlerton(legacy_db_type dbType);
-}
-
-#endif // DIAMOND_UTILS_H
